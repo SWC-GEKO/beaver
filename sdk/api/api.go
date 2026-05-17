b8d8902edcff824c7dd40cb6219fe11f5251273e
stahlco <stahl1consti2@gmail.com>
2026-05-17 16:18:03 +0200

    feat: added first-draft of possible api of sdk

---
+++ sdk/api/api.go
@@ -0,0 +1,29 @@
+package api
+
+import "context"
+
+type Event struct {
+	Headers map[string]any
+	Body    []byte
+}
+
+type FunctionContext[K comparable, S any] interface {
+	Set(key K, state S) error
+	Get(key K) (S, bool)
+	// Configuration (Connection-Function) is missing
+}
+
+type StatelessFunction interface {
+	Exec(event *Event) (*Event, error)
+}
+
+type StatefulFunction[K comparable, S any] interface {
+	Exec(ctx *FunctionContext[K, S], event *Event) (*Event, error)
+	// KeyBy(event Event) (K, error) not sure how I implement the splitter best -> virtual shards
+}
+
+type Sink interface {
+	Open(ctx context.Context) error
+	Write(ctx context.Context, event *Event) error
+	Close(ctx context.Context) error
+}
