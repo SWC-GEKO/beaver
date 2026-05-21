2b637f6e3ed00f72f512f558012d942e9307e1f1
stahlco <stahl1consti2@gmail.com>
2026-05-21 11:34:16 +0200

    feat: added health-endpoint in ControlPlane

--- cmd/controlplane/main.go
+++ cmd/controlplane/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"log"
+	"net/http"
 
 	"github.com/SWC-GEKO/beaver/pkg/controlplane"
 )
@@ -16,4 +17,17 @@ func main() {
 	log.SetPrefix("controlplane: ")
 	log.SetFlags(log.Lshortfile | log.LstdFlags)
 
+	s := server{}
+
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/health", s.health)
+
+	if err := http.ListenAndServe(addr, mux); err != nil {
+		log.Fatalln(err)
+	}
+}
+
+func (s *server) health(rw http.ResponseWriter, r *http.Request) {
+	rw.WriteHeader(http.StatusOK)
 }
