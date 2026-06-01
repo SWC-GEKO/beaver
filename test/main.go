bba485bcf75c4759f84ae9ef229db4b063898b99
stahlco <stahl1consti2@gmail.com>
2026-06-01 18:58:56 +0200

    fix: fixed example main-Method

--- test/main.go
+++ test/main.go
@@ -4,13 +4,13 @@ import (
 	"log"
 
 	"github.com/SWC-GEKO/beaver/sdk"
+	"github.com/SWC-GEKO/beaver/spec/contracts"
 )
 
 func main() {
 	rt := sdk.NewRuntime("localhost", "8080")
 
-	rt.StatelessFunction("echo", "/Users/stahlco/GolandProjects/beaver/test/echo/")
-	//rt.StatefulFunction("echo2", "/Users/stahlco/GolandProjects/beaver/test/echo2/")
+	rt.Upload("my-func", "/Users/stahlco/GolandProjects/beaver/test/echo", contracts.STATELESS)
 
 	if err := rt.Start(); err != nil {
 		log.Println(err)
